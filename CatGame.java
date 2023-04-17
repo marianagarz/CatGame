15ff4425ea009b825a4fa3e9e52aa468d039c469
Garza <mgarza@friendsbalt.org>
2023-04-17 11:22:42 -0400

    initial commit

---
+++ CatGame.java
@@ -0,0 +1,18 @@
+import edu.princeton.cs.algs4.EdgeWeightedGraph; 
+import edu.princeton.cs.algs4.Edge;
+public class CatGame{
+	EdgeWeightedGraph board; 
+	
+	public CatGame(int n){
+		//new graph of size n*n 
+		board = new EdgeWeightedGraph(n*n); 
+	}
+	
+	void markTile(int row, int col){
+		
+	}
+	//boolean marked(int row, int col); 
+	//int[] getCatTile(); 
+	//boolean catHasEscaped(); 
+	//boolean catIsTrapped(); 
+}
