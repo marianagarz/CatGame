69ba62a11e6cb5e48005fadaa2fe902f5513a108
Garza <mgarza@friendsbalt.org>
2023-04-27 11:41:42 -0400

    progress!!!!

--- CatGame.java
+++ CatGame.java
@@ -1,10 +1,13 @@
 import edu.princeton.cs.algs4.EdgeWeightedGraph; 
 import edu.princeton.cs.algs4.Edge;
+import java.util.Random;
+
 public class CatGame{
 	
 	EdgeWeightedGraph board; 
 	boolean[] marked; 
 	int size; 
+	int catPosition;
 	
 	public CatGame(int n){
 		size = n; 
@@ -12,22 +15,74 @@ public class CatGame{
 		board = new EdgeWeightedGraph(n * n + 1); 
 		marked = new boolean[n * n + 1]; 
 		
+		catPosition = index(n / 2, n / 2);
+		
 		for (int row = 1; row < n-1; row++){
 			for (int col = 1; col < n -1; col++){
-				
+				int  v = index(row, col);
+				board.addEdge(new CatEdge(v, v-1));
+				board.addEdge(new CatEdge(v, v+1));
+				board.addEdge(new CatEdge(v, v - n - 1));
+				board.addEdge(new CatEdge(v, v - n));
+				board.addEdge(new CatEdge(v, v + n));
+				board.addEdge(new CatEdge(v, v + n - 1));
 			}
 		}
+		
+		Random rand = new Random();
+		int num = rand.nextInt(n / 2);
+		while(num > 0){
+			int row = rand.nextInt(n * n); 
+			int col = rand.nextInt(n * n); 
+			while (marked(row, col)){
+				row = rand.nextInt(n * n);
+				col = rand.nextInt(n * n);
+			}
+			markTile(row, col);
+			num--; 
+		}
+		
 	}
 	
 	void markTile(int row, int col){
-		marked[index(row,col)] = true; 
+		marked[index(row,col)] = true;
+		int v = index(row, col);
+		
+		for(Edge i : board.adj(v)){
+			CatEdge e = (CatEdge) i;
+			e.changeWeight(); 
+		}
+		
+		//MOVE CAT NOW!
+		
 	}
 	
-	//boolean marked(int row, int col); 
-	//int[] getCatTile(); 
-	//boolean catHasEscaped(); 
-	//boolean catIsTrapped(); 
-	public int index(int row, int col){
-		return row * size + col
+	boolean marked(int row, int col){
+		return marked[index(row, col)];
 	}
+	
+	int[] getCatTile(){
+		int col = catPosition % size; 
+		int row = catPosition / size;
+		
+		int[] result = {row, col};
+		return result;
+		
+	}		
+	//boolean catHasEscaped(); 
+	
+	boolean catIsTrapped(){
+		for(Edge i : board.adj(catPosition)){
+			CatEdge e = (CatEdge) i;
+			if (e.weight() == 1)
+				return false; 
+		}
+		return true;
+	}
+	
+	
+	public int index(int row, int col){
+		return row * size + col;
+	}
+	
 }
